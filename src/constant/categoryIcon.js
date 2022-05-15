ee9527f502378b703a9ea30c726fe637ca9b2463
HearLing <51811652+HearLing@users.noreply.github.com>
2022-05-15 15:37:33 +0800

    Docs/social job (#24)
    * feat: 接入 antd & 新增文章 & 刷题表格
    
    * feat: 新增社招面试复盘

---
+++ src/constant/categoryIcon.js
@@ -0,0 +1,11 @@
+import { FcBusiness, FcIdea, FcCalendar, FcInspection } from 'react-icons/fc';
+
+const CATEGORY_CONTEXTS = [
+  {
+    id: 'business',
+    name: 'Business',
+    icon: FcBusiness,
+  },
+];
+
+export default CATEGORY_CONTEXTS;
